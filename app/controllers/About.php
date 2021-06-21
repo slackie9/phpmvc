629260c8edeb56727fe6b5758ee56d22f21b092b
slackie9 <isminaldiprogram@gmail.com>
2021-06-21 18:57:57 +0700

    controller study

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php 
+
+class About {
+
+    public function index($nama ='Slackie', $pekerjaan='Programmer')
+    {
+        echo "Hallo nama saya $nama saya seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
